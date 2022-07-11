7373240021c963c90ce24babcefe32f374305aa9
joinej18 <106044374+joinej18@users.noreply.github.com>
2022-07-11 00:49:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function compute()
+{
+    //Createing variables to perform operations
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    
+    var year = new Date().getFullYear()+parseInt(years);
+    
+    //Conditional operation performed to check if the principle is valid or not
+    if(principal ==undefined || principal <= 0) {
+
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+
+        else {
+            var inr = "If you deposit <mark>" + principal+" </mark><br>";
+            var ra =  `at an interest rate of <mark>${rate}%.</mark><br>`;
+            var am =  `You will receive an amount of <mark>${interest}, </mark><br>`;            
+            var ye = "in the year <mark>" + year + "</mark>";
+        
+            document.getElementById("result").innerHTML=inr+ra+am+ye;
+        
+        }  
+    
+}
+
+function updateRate() 
+{
+    //Created variable to display change in rate
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
